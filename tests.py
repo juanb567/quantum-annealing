f3d0da368154c64000324604cd0dc278c7c7aeff
juanb567 <134698857+juanb567@users.noreply.github.com>
2023-06-12 11:39:57 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ from hypothesis import settings
 from hypothesis import given
 
 @given(n = st.integers(1,4), m = st.integers(1,3))
-@settings(max_examples = 1)
 
 def test_lattice(n, m):
     
@@ -18,7 +17,6 @@ def test_lattice(n, m):
     assert abs_spins.all() == 1
 
 @given(n = st.integers(1,4), m = st.integers(1,3))
-@settings(max_examples = 1)
 
 def test_configurations(n, m):
     
@@ -27,9 +25,9 @@ def test_configurations(n, m):
         
     configurations = func.possible_configurations(n, m)
     assert len(configurations) == 2**(n*m)
-       
-@given(n = st.integers(1,4), m = st.integers(1,3))
-@settings(max_examples = 1)
+
+@settings(deadline=1000)       
+@given(n = st.integers(1,3), m = st.integers(1,3))
 
 def test_matrixH_0(n, m):
     
@@ -40,23 +38,33 @@ def test_matrixH_0(n, m):
     H = func.H_0(n, m, configurations)
     for i in range(2**(m*n)):
         assert np.sum(H[i]) == -n*m
-        
-@given(n = st.integers(1, 4), m = st.integers(1, 3), J = st.floats(-1, 1), h = st.floats(-1, 1))
-@settings(max_examples = 1)
+        for j in range(2**(m*n)):
+            assert H[i,j] == 0 or H[i,j] == -1
+
+@settings(deadline=1000)
+@given(n = st.integers(1, 3), m = st.integers(1, 3), J = st.floats(-1, 1), h = st.floats(-1, 1))
 
 def test_matrixH_1(n, m, J, h):
+    
+    """ Test to verify that the hamiltonian H_1
+        is diagonal."""
+        
     sites, spins = func.lattice(n, m)
     configurations = func.possible_configurations(n, m)
     neighbors = func.neighbors_classification(sites)
     H = func.H_1(n, m, spins, configurations, neighbors, J, h)
     for i in range(2**(m*n)):
         for j in range(2**(m*n)):
-            assert H[i, j] == 0
+            if i != j:
+                assert H[i, j] == 0
         
-@given(n = st.integers(1,4), m = st.integers(1,3))
-@settings(max_examples = 1)      
+@given(n = st.integers(1,4), m = st.integers(1,3))   
         
 def test_magnetization(n, m):
+    
+    """ Test to verify that the magnetization
+        acquires a normalized value."""
+        
     configurations = func.possible_configurations(n, m)
     for configuration in configurations:
         mag = func.magnetization(configuration)
