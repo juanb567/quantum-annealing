5f8812d1d1b6ada06536c5da6ed9f0a19c30645e
juanb567 <134698857+juanb567@users.noreply.github.com>
2023-06-14 13:23:33 +0200

    Update quantum_annealing_simulator.py

--- quantum_annealing_simulator.py
+++ quantum_annealing_simulator.py
@@ -2,13 +2,10 @@ import functions as func
 import plots
 import configparser
 
-# Create a ConfigParser object
 config = configparser.ConfigParser()
 
-# Read the variables from the text file
 config.read('configuration.txt')
 
-# Get the values of the variables
 J = int(config['J']['value'])
 h = float(config['h']['value'])
 step = float(config['step']['value'])
@@ -18,10 +15,11 @@ directory = config['directory']['value']
 
 assert n*m <= 16   # The problem is too big.
 
+############################# SIMULATION ##################################
+
 configurations, sites, b, param_lambda, magnetization_values, amplitudes_values = func.simulation(J, h, step, n, m)
 
-
-############################# GRAPHICS ##################################
+################################ PLOTS #####################################
 
 plots.plot_configuration(n, m, configurations, sites, b, directory)
 plots.plot_magnetization(param_lambda, magnetization_values, directory)
